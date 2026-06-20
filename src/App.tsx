2c51991d7460e357f39fa4fbbf23a29ab879d4a3
starlightaris <starlightaris@gmail.com>
2026-06-20 12:13:32 +0530

    Revert "Merge pull request #1 from starlightaris/Yeshmi"
    This reverts commit 29425ae3175d5658295696f120810237786e4a66, reversing
    changes made to e7b405c34a80e591943c87de494fb201d3ae4bea.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './pages/auth/LoginScreen';
-import DriverHome from './pages/DriverHome';
-import PassengerHome from './pages/PassengerHome';
-
-export type RootStackParamList = {
-  Login: undefined;
-  DriverHome: undefined;
-  PassengerHome: undefined;
-};
-
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
