f78d5712b63bd6ab8215c7725a6f7c9adcc44696
Anhaj0 <anhaj02003@gmail.com>
2026-06-21 13:32:37 +0530

    Fix App.tsx to use named import for Attendance screen temporarily

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Attendance } from './src/pages/passenger/Attendance';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // Temporary setup for testing Attendance screen. 
+  // The final app will use the correct app navigation from src/navigation/RootNavigator
+  return <Attendance />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
