565e5cd0a2400e0d6a6389e88f2d344a993e23ff
Shalin <shalinheenkenda@gmail.com>
2026-06-24 19:08:17 +0530

    Basic Map UI

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import EditLocationScreen from './src/pages/passenger/EditLocationScreen';
+import ProfileScreen from './src/pages/passenger/ProfileScreen';
 
 export default function App() {
-  return (
+  /*return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  );*/
+  return (<ProfileScreen />
   );
 }
 
